c4d6f244c2af26dcf2deddb3ac586d433f9c80d0
ellamartirosyan <ellamartirosyan@gmail.com>
2026-01-25 18:38:54 +0200

    chore: fix IconButton

--- src/Common/Footer/Footer.tsx
+++ src/Common/Footer/Footer.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { useIntl } from 'react-intl';
 import { Box } from '@map-colonies/react-components';
-import { IconButton } from '@map-colonies/react-core';
+import { IconButton, Tooltip } from '@map-colonies/react-core';
 
 import './Footer.css';
 
 const Footer: React.FC = (): JSX.Element => {
+  const intl = useIntl();
+  
   return (
     <Box className="Footer">
-      <IconButton className="icon mc-icon-Next" onClick={(e): void => { e.preventDefault(); e.stopPropagation(); }} />
-      <IconButton className="icon mc-icon-Back" onClick={(e): void => { e.preventDefault(); e.stopPropagation(); }} />
+      <Tooltip content={intl.formatMessage({ id: 'footer.back' })}>
+        <IconButton className="icon mc-icon-Next" onClick={(e): void => { e.preventDefault(); e.stopPropagation(); }} />
+      </Tooltip>
+      <Tooltip content={intl.formatMessage({ id: 'footer.next' })}>
+        <IconButton className="icon mc-icon-Back" onClick={(e): void => { e.preventDefault(); e.stopPropagation(); }} />
+      </Tooltip>
     </Box>
   );
 };
